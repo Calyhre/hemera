eda083cbacaca7c0a98ba30d4016e8d0088ec956
Calyhre <calyhre@gmail.com>
2015-12-07 13:55:30 +0100

    Disable french

--- config.rb
+++ config.rb
@@ -2,7 +2,8 @@ require 'slim'
 Slim::Engine.disable_option_validator!
 
 activate :dotenv
-activate :i18n, mount_at_root: false, langs: %i(en fr), path: '/:locale/'
+# activate :i18n, mount_at_root: false, langs: %i(en fr), path: '/:locale/'
+activate :i18n, mount_at_root: false, langs: %i(en), path: '/:locale/'
 redirect 'index.html', to: 'en/index.html'
 # I18n.default_locale = :en
 
