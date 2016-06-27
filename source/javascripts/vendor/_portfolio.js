7667a16b4576e63a2764ad07ae1f99d2d6f29d38
valentingalmand <valentin@vals-mbp.home>
2016-06-27 15:42:31 +0200

    Update portfolio

--- source/javascripts/vendor/_portfolio.js
+++ source/javascripts/vendor/_portfolio.js
@@ -1,34 +1,34 @@
-var logos = document.querySelectorAll('.portfolio-section')
-var modals = document.querySelectorAll('.portfolio-modal')
-var overlay = document.querySelector('.overlay')
+    var portfolio_logos = document.querySelectorAll('.portfolio-section');
+    var portfolio_modals = document.querySelectorAll('.portfolio-modal');
+    var portfolio_overlay = document.querySelector('.overlay');
 
-for (var i = 0; i < logos.length; i++) {
-    logos[i].addEventListener('click', function(evt) {
-        evt.preventDefault
-        var namePortfolio = this.className.split('--')[1]
+    for (var i = 0; i < portfolio_logos.length; i++) {
+        portfolio_logos[i].addEventListener('click', function(evt) {
+            evt.preventDefault;
+            var portfolio_name = this.className.split('--')[1];
 
-        for (var i = 0; i < modals.length; i++) {
-            var nameModal = modals[i].className.split('--')[1]
-            if (nameModal === namePortfolio) {
-                modals[i].classList.add('portfolio-modal--active')
-              overlay.classList.add('overlay--active')
+            for (var i = 0; i < portfolio_modals.length; i++) {
+                var portfolio_modal = portfolio_modals[i].className.split('--')[1];
+                if (portfolio_modal === portfolio_name) {
+                    portfolio_modals[i].classList.add('portfolio-modal--active');
+                    portfolio_overlay.classList.add('overlay--active');
+                }
             }
-        }
-    })
-}
-
-function hideModal() {
-    var modalActive = document.querySelector('.portfolio-modal--active')
-		overlay.classList.remove('overlay--active')
-    modalActive.classList.remove('portfolio-modal--active')
-}
-
-overlay.addEventListener('click', function() {
-    hideModal()
-})
-
-document.onkeydown = function(e) {
-    if (e.keyCode === 27) {
-        hideModal()
+        });
     }
-};
+
+    function hideModal() {
+        var portfolio_modal__active = document.querySelector('.portfolio-modal--active');
+        portfolio_overlay.classList.remove('overlay--active');
+        portfolio_modal__active.classList.remove('portfolio-modal--active');
+    }
+
+    portfolio_overlay.addEventListener('click', function() {
+        hideModal();
+    });
+
+    document.onkeydown = function(e) {
+        if (e.keyCode === 27) {
+            hideModal();
+        }
+    };
