6bd44b2e210329f8e16480beaca42aad9928b240
valentingalmand <valentin@vals-mbp.home>
2016-06-28 17:17:36 +0200

    Add changing background modal and on hover

--- source/javascripts/vendor/_portfolio.js
+++ source/javascripts/vendor/_portfolio.js
@@ -7,14 +7,32 @@ for (var i = 0; i < portfolio_logos.length; i++) {
         evt.preventDefault;
         var portfolio_name = this.className.split('--')[1];
 
+        var colorsArray = ['#F6A623','#00DF94','#FD587B','#88D1FC']
+
+        var randomColor = colorsArray[Math.floor(Math.random() * colorsArray.length)];
+
         for (var i = 0; i < portfolio_modals.length; i++) {
             var portfolio_modal = portfolio_modals[i].className.split('--')[1];
             if (portfolio_modal === portfolio_name) {
                 portfolio_modals[i].classList.add('portfolio-modal--active');
+                portfolio_modals[i].style.backgroundColor = randomColor;
                 portfolio_overlay.classList.add('overlay--active');
+
             }
         }
     });
+    portfolio_logos[i].addEventListener('mouseenter', function(evt) {
+        var shapesArray = ['circle', 'triangle', 'hamburger', 'cutdot'];
+        var randomShape = shapesArray[Math.floor(Math.random() * shapesArray.length)];
+        this.style.background = 'url("/images/join-deco--' + randomShape + '.svg") no-repeat center center';
+        this.style.backgroundSize = 'contain';
+
+    });
+    portfolio_logos[i].addEventListener('mouseleave', function(evt) {
+        this.style.background = '';
+        this.style.backgroundSize = '';
+    });
+
 }
 
 function hideModal() {
